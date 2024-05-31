78b750f16485724ae4cf310425b6df06f1b723d6
Euraxluo <euraxluo@outlook.com>
2024-06-01 04:55:05 +0800

    wrapper

---
+++ wrapper/publish.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# out=$(sui client publish --skip-dependency-verification --skip-fetch-latest-git-deps)
+out=$(sui client publish)
+
+if [ $? -eq 0 ]; then
+    echo "$out"
+else
+    echo "$out"
+    echo "Error: sui client publish failed."
+    exit 1
+fi
+
+package_id=$(echo "$out"| sed -n '/Published Objects/,/Version/ s/.*PackageID: //p' )
+
+echo ""
+echo "──────────────────────────────────────────────────────────────────────────────────────────────────────────────────"
+echo ""
+
+echo "PackageID:"
+echo "$package_id"
+
+echo ""
+echo "──────────────────────────────────────────────────────────────────────────────────────────────────────────────────"
+echo ""
